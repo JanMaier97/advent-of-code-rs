19f56e4b61bfb0a9539774c3d8d854ff7b9e7373
Jan Maier <13823649+JanMaier97@users.noreply.github.com>
2024-12-17 00:49:09 +0100

    Revert "try out"
    This reverts commit 5ed39d7f82283621c99e34a4c8aeca5584ec195f.

--- src/year_2024/day_16/part_1.rs
+++ src/year_2024/day_16/part_1.rs
@@ -1,10 +1,9 @@
 
-use std::{alloc::GlobalAlloc, collections::{HashMap, HashSet}, u64};
+use std::{collections::{HashMap, HashSet}, u64};
 
 use anyhow::{anyhow, bail, Result};
 use itertools::Itertools;
 use macros::aoc_solver;
-use rayon::iter::{empty, Empty};
 
 use crate::{common::{math_2d::{Grid, Point, PointIdx, Vec2}, parsing::parse_grid}, MyResult};
 
@@ -61,7 +60,7 @@ fn find_lowest_cost(grid: &Grid<Tile>, visited: &HashMap<Point<i32>, u64>, curre
         return current_cost;
     }
 
-    let next_points: Vec<NextPoint> = get_next_points(grid, current_pos, current_dir);
+    let next_points = get_next_points(grid, visited, current_pos, current_dir);
     if next_points.is_empty() {
         return u64::MAX;
     }
@@ -89,7 +88,7 @@ struct NextPoint {
     cost: u64, 
 }
 
-fn get_next_points(grid: &Grid<Tile>, pos: Point<i32>, current_dir: Vec2<i32>) -> Vec<NextPoint> {
+fn get_next_points(grid: &Grid<Tile>, visited: &HashMap<Point<i32>, u64>, pos: Point<i32>, current_dir: Vec2<i32>) -> Vec<NextPoint> {
     let mut points = Vec::new();
 
     let next_pos = pos + current_dir;
@@ -114,8 +113,24 @@ fn get_next_points(grid: &Grid<Tile>, pos: Point<i32>, current_dir: Vec2<i32>) -
         });
     }
 
+    if points.len() == 0 {
+        // println!("{:?} is a dead end", pos);
+        return Vec::new();
+    }
 
-    points
+    let filtered = points
+        .into_iter()
+        .filter(|p| !visited.contains_key(&p.pos))
+        .collect_vec();
+
+    if filtered.is_empty() {
+        // println!("{:?} ends in a loop", pos);
+        return Vec::new();
+    }
+
+    // println!("next positions to try: {:?}", filtered);
+
+    filtered
 }
 
 
@@ -162,141 +177,6 @@ fn print_map(grid: &Grid<Tile>, pos: Point<i32>, path: &[Point<i32>]) {
         }
         println!()
     }
-}
-
-fn find_shortes_crossing_path(map: HashMap<Point<i32>, HashSet<Point<i32>>>, start: Point<i32>, end: Point<i32>) -> Vec<Point<i32>> {
-    let mut visited: HashSet<Point<i32>> = HashSet::new();
-    let mut paths: Vec<Vec<Point<i32>>> = vec![vec![start]];
-
-
-    loop {
-        let Some(path) = paths.pop() else {
-            break;
-        };
-
-        let current_pos = *path.last().unwrap();
-
-        if visited.contains(&current_pos) {
-            continue;
-        }
-
-        let neighbours = map.get(&current_pos).unwrap();
-
-        for neighbour in neighbours {
-            let mut new_path = path.clone();
-            new_path.push(current_pos);
-            paths.push(new_path);
-
-            neighbour == end {
-                return new_path;
-            };
-
-
-            
-            visited.insert(current_pos);
-        }
-
-    }
-
-
-}
-
-
-
-fn collect_crossings(grid: &Grid<Tile>) -> HashMap<Point<i32>, HashSet<Point<i32>>> {
-    let crossings = find_crossings(grid);
-
-    let mut mapping = HashMap::new();
-    for point in crossings.iter().cloned() {
-        let reachable = find_reachable_crossings(grid, &crossings, point);
-        mapping.insert(point, reachable);
-
-    }
-
-    mapping
-}
-
-fn find_reachable_crossings(grid: &Grid<Tile>, crossings: &HashSet<Point<i32>>, start_pos: Point<i32>) -> HashSet<Point<i32>> {
-    let s1 = find_reachable_crossings_in_dir(grid, crossings, start_pos, Vec2::UP);
-    let s2 = find_reachable_crossings_in_dir(grid, crossings, start_pos, Vec2::DOWN);
-    let s3 = find_reachable_crossings_in_dir(grid, crossings, start_pos, Vec2::LEFT);
-    let s4 = find_reachable_crossings_in_dir(grid, crossings, start_pos, Vec2::RIGHT);
-
-    let mut result = HashSet::from(s1);
-    result.extend(s2);
-    result.extend(s3);
-    result.extend(s4);
-
-    result
-}
-
-fn find_reachable_crossings_in_dir(grid: &Grid<Tile>, crossings: &HashSet<Point<i32>>, start_pos: Point<i32>, direction: Vec2<i32>) -> HashSet<Point<i32>> {
-    let mut reachable_crossings = HashSet::new();
-    let mut current_pos = start_pos;
-    loop {
-        current_pos += direction;
-        if crossings.contains(&current_pos) {
-            reachable_crossings.insert(current_pos);
-        }
-
-        let tile = *grid.get_at(current_pos).unwrap();
-        if tile == Tile::Wall {
-            break;
-        }
-    }
-
-    reachable_crossings
-}
-
-
-fn find_crossings(grid: &Grid<Tile>) -> HashSet<Point<i32>> {
-    let mut crossings = HashSet::new();
-    for y in 0..grid.dim().height {
-        for x in 0..grid.dim().width {
-            let point: Point<i32> = Point{x: x as i32, y: y as i32};
-            let tile = *grid.get_at(point).unwrap();
-            if tile == Tile::Wall {
-                continue;
-            }
-
-            if tile == Tile::Start || tile == Tile::End || is_crossing(grid, point) {
-                crossings.insert(point);
-            }
-        }
-    }
-
-    crossings
-}
-
-fn is_crossing(grid: &Grid<Tile>, pos: Point<i32>) -> bool {
-    let positions = vec![
-        pos + Vec2::UP,
-        pos + Vec2::DOWN,
-        pos + Vec2::RIGHT,
-        pos + Vec2::LEFT,
-    ];
-    let neighbours = positions.iter().map(|p| *grid.get_at(*p).unwrap()).collect_vec();
-
-    let free_tiles = neighbours.iter().filter(|t| **t != Tile::Wall).count();
-    if free_tiles == 1 {
-        return false;
-    }
-
-    if free_tiles > 3 {
-        return true;
-    }
-
-    // crossing only exists for 2 free tiles
-    // if they are not opsosite of each other
-    if neighbours[0] != Tile::Wall && neighbours[1] != Tile::Wall {
-        return false;
-    }
-
-    if neighbours[2] != Tile::Wall && neighbours[3] != Tile::Wall {
-        return false;
-    }
-
-    return true;
 }
 
 #[cfg(test)]
