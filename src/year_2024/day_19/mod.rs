23af2ac289dbe36c6eab37b176fc070499f992ae
Jan Maier <13823649+JanMaier97@users.noreply.github.com>
2024-12-19 14:54:48 +0100

    refactoring

--- src/year_2024/day_19/mod.rs
+++ src/year_2024/day_19/mod.rs
@@ -1,6 +1,52 @@
 use anyhow::{bail, Result};
+use itertools::Itertools;
+use regex::Regex;
 
 mod part_1;
 mod part_2;
 
 const INPUT: &str = include_str!("input.txt");
+
+struct PuzzleInput {
+    patterns: Vec<String>,
+    designs: Vec<String>,
+}
+
+
+fn get_valid_designs(input: &str) -> Result<Vec<String>> {
+    let puzzle_input = parse_input(input)?;
+
+    let pattern = puzzle_input.patterns.join("|");
+    let reg = Regex::new(format!("^({})+$", pattern).as_str())?;
+
+    let designs = puzzle_input
+        .designs
+        .into_iter()
+        .filter(|design| reg.is_match(design))
+        .collect_vec();
+
+    Ok(designs)
+}
+
+fn parse_input(input: &str) -> Result<PuzzleInput> {
+    let blocks = input.split("\r\n\r\n").collect_vec();
+
+    if blocks.len() != 2 {
+        bail!("Expected 2 blocks");
+    }
+
+    let res = PuzzleInput {
+        patterns: parse_patterns(blocks[0]),
+        designs: parse_designs(blocks[1]),
+    };
+
+    Ok(res)
+}
+
+fn parse_designs(input: &str) -> Vec<String> {
+    input.lines().map(|l| l.to_string()).collect_vec()
+}
+
+fn parse_patterns(line: &str) -> Vec<String> {
+    line.split(", ").map(|p| p.to_string()).collect_vec()
+}
--- src/year_2024/day_19/part_1.rs
+++ src/year_2024/day_19/part_1.rs
@@ -1,50 +1,12 @@
-use anyhow::{bail, Result};
-use itertools::Itertools;
+use anyhow::Result;
 use macros::aoc_solver;
-use regex::Regex;
 
-struct PuzzleInput {
-    patterns: Vec<String>,
-    designs: Vec<String>,
-}
+use super::get_valid_designs;
 
 #[aoc_solver(2024, 19, 1, super::INPUT)]
 fn solve(input: &str) -> Result<String> {
-    let puzzle_input = parse_input(input)?;
-
-    let pattern = puzzle_input.patterns.join("|");
-    let reg = Regex::new(format!("^({})+$", pattern).as_str())?;
-
-    let valid_designs = puzzle_input
-        .designs
-        .iter()
-        .filter(|design| reg.is_match(design))
-        .count();
-
-    Ok(valid_designs.to_string())
-}
-
-fn parse_input(input: &str) -> Result<PuzzleInput> {
-    let blocks = input.split("\r\n\r\n").collect_vec();
-
-    if blocks.len() != 2 {
-        bail!("Expected 2 blocks");
-    }
-
-    let res = PuzzleInput {
-        patterns: parse_patterns(blocks[0]),
-        designs: parse_designs(blocks[1]),
-    };
-
-    Ok(res)
-}
-
-fn parse_designs(input: &str) -> Vec<String> {
-    input.lines().map(|l| l.to_string()).collect_vec()
-}
-
-fn parse_patterns(line: &str) -> Vec<String> {
-    line.split(", ").map(|p| p.to_string()).collect_vec()
+    let desings = get_valid_designs(input)?;
+    Ok(desings.len().to_string())
 }
 
 #[cfg(test)]
