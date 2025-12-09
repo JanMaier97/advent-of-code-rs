562dcf1d67786d8f1d7f174ee04b3a0cff7826ac
Jan Maier <13823649+JanMaier97@users.noreply.github.com>
2025-12-09 01:04:05 +0100

    Solve year 2025 day 08 part 2

--- src/year_2025/day_08/mod.rs
+++ src/year_2025/day_08/mod.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use macros::aoc_solver;
 
-use anyhow::{bail, Context, Result};
+use anyhow::{bail, ensure, Context, Result};
 
 static INPUT: &str = include_str!("input.txt");
 
@@ -19,19 +19,38 @@ impl Point3 {
     }
 }
 
+struct CircuitResult {
+    sizes: Vec<usize>,
+    last_pair: (Point3, Point3)
+}
+
 #[aoc_solver(2025, 8, 1, INPUT)]
 fn solve_part_1(input: &str) -> Result<String> {
     compute_result_with_pair_count(input, 1000)
 }
 
+#[aoc_solver(2025, 8, 2, INPUT)]
+fn solve_part_2(input: &str) -> Result<String> {
+    let points = parse_points(input)?;
+    let sorted_pairs = get_pairs_sorted(&points);
+    let circuits = get_circuits_sizes(&sorted_pairs)?;
+
+    ensure!(circuits.sizes.len() == 1);
+
+    let (l, r) = circuits.last_pair;
+    let result = l.x * r.x;
+    Ok(result.to_string())
+}
+
 fn compute_result_with_pair_count(input: &str, count: usize) -> Result<String> {
     let points = parse_points(input)?;
     let sorted_pairs = get_pairs_sorted(&points);
-    let mut circuits = get_circuits_sizes(&sorted_pairs[0..count])?;
+    let circuits = get_circuits_sizes(&sorted_pairs[0..count])?;
 
-    circuits.sort();
+    let mut sizes = circuits.sizes.clone();
+    sizes.sort();
 
-    let result = circuits
+    let result = sizes
         .into_iter()
         .rev()
         .take(3)
@@ -41,9 +60,9 @@ fn compute_result_with_pair_count(input: &str, count: usize) -> Result<String> {
     Ok(result.to_string())
 }
 
-fn get_circuits_sizes(point_pairs: &[(Point3, Point3)]) -> Result<Vec<usize>> {
+fn get_circuits_sizes(point_pairs: &[(Point3, Point3)]) -> Result<CircuitResult> {
     let mut circuits: Vec<HashSet<Point3>> = Vec::new();
-
+    let mut last_pair = point_pairs[0];
     for (left_point, right_point) in point_pairs {
         let left_circuit_idx = circuits.iter().position(|c| c.contains(left_point));
         let right_circuit_idx = circuits.iter().position(|c| c.contains(right_point));
@@ -56,14 +75,17 @@ fn get_circuits_sizes(point_pairs: &[(Point3, Point3)]) -> Result<Vec<usize>> {
             }
             (None, Some(idx)) => {
                 circuits[idx].insert(*left_point);
+                last_pair = (*left_point, *right_point);
             }
             (Some(idx), None) => {
                 circuits[idx].insert(*right_point);
+                last_pair = (*left_point, *right_point);
             }
             (Some(lidx), Some(ridx)) => {
                 if lidx == ridx {
                     continue;
                 }
+                last_pair = (*left_point, *right_point);
                 // could be done with vec::swap_remove -> indices need to be mainted correctly then
                 let points_to_merge = circuits[ridx].clone();
                 circuits[lidx].extend(points_to_merge);
@@ -72,7 +94,12 @@ fn get_circuits_sizes(point_pairs: &[(Point3, Point3)]) -> Result<Vec<usize>> {
         }
     }
 
-    Ok(circuits.iter().map(|c| c.len()).collect::<Vec<_>>())
+    let sizes = circuits.iter().map(|c| c.len()).collect::<Vec<_>>();
+
+    Ok(CircuitResult { 
+        sizes,
+        last_pair,
+    })
 }
 
 fn get_pairs_sorted(points: &HashSet<Point3>) -> Vec<(Point3, Point3)> {
@@ -127,8 +154,20 @@ mod tests {
     }
 
     #[test]
+    fn solve_example_part_2() {
+        let result = super::solve_part_2(include_str!("example.txt")).unwrap();
+        assert_eq!(result, "25272");
+    }
+
+    #[test]
     fn solve_part_1() {
         let result = super::solve_part_1(super::INPUT).unwrap();
         assert_eq!(result, "121770");
+    }
+
+    #[test]
+    fn solve_part_2() {
+        let result = super::solve_part_2(super::INPUT).unwrap();
+        assert_eq!(result, "7893123992");
     }
 }
